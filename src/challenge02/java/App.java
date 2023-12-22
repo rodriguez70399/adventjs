ce2b0ef74d84f1d930e52c925e60d41b56dc67b6
rodriguez70399 <rodriguez70399@gmail.com>
2023-12-22 11:24:44 +0100

    Challenge 02 completed

--- src/challenge02/java/App.java
+++ src/challenge02/java/App.java
@@ -1,5 +1,23 @@
 package challenge02.java;
 
-public class App {
-    
+import java.util.stream.Stream;
+
+public class App 
+{
+    public static String[] manufacture(String[] gifts, String materials) 
+    {
+        return Stream.of(gifts)
+            .filter(gift -> gift.chars()
+                .mapToObj(letter -> String.valueOf((char)letter))
+                .allMatch(letter -> materials.contains(letter)))
+            .toArray(String[]::new);
+    }
+
+    public static void main(String[] args) 
+    {
+        String[] gifts = {"tren", "oso", "pelota"};
+        String materials = "tronesa";
+        Stream.of(manufacture(gifts, materials))
+        .forEach(result -> System.out.println(result));
+    }    
 }
---
+++ src/challenge02/js/main.js
@@ -0,0 +1,15 @@
+let manufacture = (gifts, materials) =>
+{
+    return gifts
+    .filter(([...gift]) => gift
+    .every(letter => materials
+    .includes(letter)));
+}
+
+const gifts = ['tren', 'oso', 'pelota']
+const materials = 'tronesa'
+
+console.log(manufacture(gifts, materials)) // ["tren", "oso"]
+// 'tren' SÍ porque sus letras están en 'tronesa'
+// 'oso' SÍ porque sus letras están en 'tronesa'
+// 'pelota' NO porque sus letras NO están en 'tronesa'
